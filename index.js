a0a8782154f7f0a45d95c9db3cf963b2efda3e5f
Alexa Schaeffer <alps231@gmail.com>
2018-06-25 03:34:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,19 @@ var kittens = ["Milo", "Otis", "Garfield"]
 function destructivelyAppendKitten(name) {
   kittens.push(name)
 }
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name) 
+}
+
+function destructivelyRemoveLastKitten() {
+kittens.pop()
+}
+
+function destucivelyRemoveFirstKitten() {
+  kittens.shift()
+}
+
+
+  
+}
